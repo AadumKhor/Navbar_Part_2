b5fc5ee90618816d43635734a3174d1569bc4250
Aayush Malhotra <37381075+AadumKhor@users.noreply.github.com>
2019-05-25 01:55:53 +0530

    Added basic functionality

--- barnavbar/lib/main.dart
+++ barnavbar/lib/main.dart
@@ -53,12 +53,18 @@ class BarNavbar extends StatefulWidget {
   _BarNavbarState createState() => _BarNavbarState(selectedIndex);
 }
 
-class _BarNavbarState extends State<BarNavbar> {
+class _BarNavbarState extends State<BarNavbar>
+    with SingleTickerProviderStateMixin {
+  Size _size;
+
+  double squeezLength = 10.0; //squeezLength of the bar
+  double fullLength = 25.0; // full Length of the bar
   int selectedIndex; // selected currently
   int newIndex; // new one that has to be updated
 
   Animation<double> posAnim; //animation of the selected bar
-  Animation<Color> colorAnim; //color change
+  Animation<double> colorAnim;
+  //color change** for the time being double but I think color would be better
   Animation<double> squeezAnim; //squeez to add a good touch
   Animation<double>
       stretchAnim; //coz every action has equal and opposite reaction
@@ -70,7 +76,98 @@ class _BarNavbarState extends State<BarNavbar> {
 
   @override
   void initState() {
+    controller = new AnimationController(
+        vsync: this, duration: Duration(milliseconds: 500));
+    posAnim = new Tween<double>(
+            begin: selectedIndex * 1.0, end: (selectedIndex + 1) * 1.0)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.bounceIn));
+    colorAnim = new Tween<double>(
+            begin: selectedIndex * 1.0, end: (selectedIndex + 1) * 1.0)
+        .animate(CurvedAnimation(parent: controller, curve: Curves.bounceIn));
+    squeezAnim = new Tween(begin: fullLength, end: squeezLength).animate(
+        CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.0, 0.15, curve: Curves.ease)));
+    stretchAnim = new Tween(begin: squeezLength, end: fullLength).animate(
+        CurvedAnimation(
+            parent: controller,
+            curve: Interval(0.0, 0.15, curve: Curves.ease)));
+    controller.addListener(() => setState(() {}));
+    controller.addStatusListener((status) {
+      if (controller.isCompleted) {
+        selectedIndex = newIndex;
+        controller.reset();
+      }
+    });
     super.initState();
+  }
+
+  List<Widget> smallIcons() {
+    List<Widget> icons = [];
+    for (int i = 0; i < widget.icons.length; i++) {
+      var a = Expanded(
+        child: Container(
+          child: InkResponse(
+            onTap: () {
+              //   tapped(i, true);
+            },
+            child: Opacity(
+              opacity: getOpacityForIndex(i),
+              child: Container(
+                height: kBottomNavigationBarHeight * 1.6,
+                width: _size.width / 5,
+                child: Icon(
+                  widget.icons[i],
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+      icons.add(a);
+    }
+    return icons;
+  }
+
+  //method copied from stackoverflow
+  double getOpacityForIndex(int index) {
+    if (controller.isAnimating) {
+      var dist = (index - posAnim.value).abs();
+      if (dist >= 1) {
+        return 1;
+      } else {
+        return dist;
+      }
+    } else {
+      return selectedIndex == index ? 0 : 1;
+    }
+  }
+
+  // to check and update the interactions
+  void tapped(int index, bool userInteraction) {
+    if (userInteraction) {
+      widget.tapCallback(index);
+    }
+    newIndex = index;
+    posAnim = Tween<double>(begin: selectedIndex * 1.0, end: index * 1.0)
+        .animate(CurvedAnimation(
+      parent: controller,
+      curve: Curves.ease,
+    ));
+
+    controller.forward();
+  }
+
+  //function to update the widget
+  @override
+  void didUpdateWidget(BarNavbar oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.selectedIndex == widget.selectedIndex) {
+      return;
+    }
+    tapped(widget.selectedIndex, false);
+    // function to check if the button is tapped
   }
 
   @override
